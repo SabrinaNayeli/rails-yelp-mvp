6b044beb308f3089121d897c4f06591750f6beca
Nayeli Hamzaoui <hamzaoui.sbn@gmail.com>
2018-08-15 17:46:43 +0200

    restaurants views ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,11 +4,21 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    Review.create(params[:restaurant_id])
+  end
 end
