69f9035e87de948150c6f6e41fb475c4b7067e90
Nayeli Hamzaoui <hamzaoui.sbn@gmail.com>
2018-08-15 15:10:28 +0200

    index ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Chez toto', address: '1 rue des lilas, Lyon', phone_number: 0726564321, category: 'chinese')
+Restaurant.create(name: 'Chez tata', address: '2 rue des lilas, Lyon', phone_number: 0726564322, category: 'italian')
+Restaurant.create(name: 'Chez titi', address: '3 rue des lilas, Lyon', phone_number: 0726564323, category: 'french')
+Restaurant.create(name: 'Chez tete', address: '4 rue des lilas, Lyon', phone_number: 0726564324, category: 'belgian')
+Restaurant.create(name: 'Chez tyty', address: '5 rue des lilas, Lyon', phone_number: 0726564325, category: 'japanese')
